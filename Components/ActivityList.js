e25cf99f36680a27fa47ddc6775f0b2bf4877306
cassiesong1225 <cassiesong1225@hotmail.com>
2024-02-24 12:45:35 -0800

    fetch activity data from firestore

--- Components/ActivityList.js
+++ Components/ActivityList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import ActivityItem from './ActivityItem';
+import { fetchActivitiesFromDB } from '../firebase-files/fireStoreHelper';
 
-export default function ActivityList({ activities,filter }) {
+
+export default function ActivityList({  filter }) {
+    const [activities, setActivities] = useState([]);
     const filteredActivities = filter === 'special'
         ? activities.filter(activity => activity.isSpecial)
         : activities;
     const renderItem = ({ item }) => <ActivityItem activity={item} />;
+
+    useEffect(() => {
+    const getActivities = async () => {
+      const fetchedActivities = await fetchActivitiesFromDB();
+      setActivities(fetchedActivities);
+    };
+
+    getActivities();
+  }, []); 
     
     return (
         <View >
--- firebase-files/fireStoreHelper.js
+++ firebase-files/fireStoreHelper.js
@@ -1,4 +1,4 @@
-import { collection, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore";
+import { collection, addDoc, doc, deleteDoc, setDoc,getDocs } from "firebase/firestore";
 import { database } from "./firebaseSetup";
 
 export async function addActivityToDB(activityData) {
@@ -8,4 +8,18 @@ export async function addActivityToDB(activityData) {
   } catch (err) {
     console.log(err);
   }
+}
+
+export async function fetchActivitiesFromDB() {
+  const activities = [];
+  try {
+    const querySnapshot = await getDocs(collection(database, "activities"));
+    querySnapshot.forEach((doc) => {
+     
+      activities.push({ id: doc.id, ...doc.data() });
+    });
+  } catch (err) {
+    console.error("Error fetching activities: ", err);
+  }
+  return activities;
 }
