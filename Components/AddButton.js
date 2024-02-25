87bcc8ccf1c28970cb9e5d53a777b2b995bbf7bb
cassiesong1225 <cassiesong1225@hotmail.com>
2024-02-24 20:42:55 -0800

    update addbutton title to plus icon

--- Components/AddButton.js
+++ Components/AddButton.js
@@ -1,13 +1,33 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
+import PressableButton from './PressableButton'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-export default function AddButton({ navigation}) {
+
+export default function AddButton({ navigation }) {
+  
+  function AddButton() {
+     navigation.navigate('AddAnActivity')
+   
+  }
+  const PlusIcon = () => <AntDesign name="plus" size={30} color="white" />;
 
   return (
-    <View>
-        <Button title="Add" onPress={() => navigation.navigate('AddAnActivity')} color = 'orange'/>
+    <View >
+      <PressableButton
+        customStyle={styles.AddButton} 
+        onPressFunction={AddButton} >
+        <PlusIcon />  
+      </PressableButton>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+  AddButton: {
+    backgroundColor: 'transparent',
+    alignItems: 'flex-end', 
+
+  }
+})
